76f7c7b595f47c4e6c12dbe5a4ec8d88b86953a1
Lin Chen <linchenkaren@gmail.com>
2022-01-12 20:17:55 -0600

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,14 @@
+
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'pretty good'
+    }, {
+        id: 2,
+        rating: 9,
+        text: 'not bad'
+    }
+]
+
+export default FeedbackData
